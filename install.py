7a2c590eccadbd355d9b34859052caee34d656dd
vasuiyen <61954526+vasuiyen@users.noreply.github.com>
2021-04-24 19:10:20 -0400

    Created using Colaboratory

---
+++ install.py
@@ -0,0 +1,47 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled6.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMS0o2bIOBdwEHtErglmoEX",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/vasuiyen/CS230_Project/blob/main/install.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "0CN9Is6QJAcw"
+      },
+      "source": [
+        "import os\n",
+        "import tensorflow as tf\n",
+        "import numpy as np\n",
+        "from datetime import datetime\n",
+        "from tensorflow.keras import layers\n",
+        "from keras.callbacks import ModelCheckpoint"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
